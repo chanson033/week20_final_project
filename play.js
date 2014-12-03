41e4225c6daa0d0726e5dfd22322d731d3312540
chanson033 <chanson033@gmail.com>
2014-12-02 23:34:26 -0800

    trying to grab html with jquery

--- play.js
+++ play.js
@@ -1,30 +1,35 @@
 
 $(document).ready(function() {
 
-	var day = 1; // start on day 1 and add to it
+	var days = [];
 	var dailyTotal;
+	var grandTotal;
 
 
   // Set up a submit handler so that data is passed when the form is submitted
   $("form.dailyscore").submit(function() {
-    
-  // take the form values and store in an array, yes = 2 pts, no = 0 pts
+  
+  // take the form values and store as an object in an array, for workout and stretch yes=2, for supp, water, other, comments yes=1, no=0 (store points or just true false?)
 
-  // add up the daily total and push to the array
+  // add up the daily total and push to the array as a key value pair for this object
 
   // update grand total for 28 day period
+  $(".myTotal").append ("<h1>something</h1>"); // if I wasn't using mulitple pages, I think this would work
 
   //reset the form fields (this will happen automatically with return I think)
 
-  // the day number progresses
+  // the day number on the html page progresses
 
   // push info to Firebase
 
-  // display values on my scoreboard, including comments
+  // display all form values on my scoreboard, including comments and grandTotal
+  $( ".day1 td:nth-of-type(2)" )
+		.replaceWith ( "5" );
+		//.addClass( "nth" ); // only do this if I need to remove the color for zero points
 
-  // when day =28, display button to start over 
+  // when day =29, display button to start over and hide form
 
-    //return false; not in my case - I WANT it to return to refresh the form
+   //return false; NOT in my case - I WANT it to return to refresh the form
   });
 
 
