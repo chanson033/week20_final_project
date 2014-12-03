23464895d668c1d8a18efbd9ab05d7081c1e390d
chanson033 <chanson033@gmail.com>
2014-12-02 21:29:52 -0800

    just getting started on JS

--- play.js
+++ play.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function() {
+
+	var day = 1; // start on day 1 and add to it
+	var dailyTotal;
+
+
   // Set up a submit handler so that data is passed when the form is submitted
-  $("form.search").submit(function() {
+  $("form.dailyscore").submit(function() {
     refresh();
 
   
