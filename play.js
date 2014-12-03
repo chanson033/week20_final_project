18876976e067fce746b3e234d344d901b5ee394b
chanson033 <chanson033@gmail.com>
2014-12-02 21:38:36 -0800

    pseudo code in JS

--- play.js
+++ play.js
@@ -7,12 +7,28 @@ $(document).ready(function() {
 
   // Set up a submit handler so that data is passed when the form is submitted
   $("form.dailyscore").submit(function() {
-    refresh();
+    
+  // take the form values and store in an array, yes = 2 pts, no = 0 pts
 
-  
+  // add up the daily total and push to the array
+
+  // update grand total for 28 day period
+
+  //reset the form fields (this will happen automatically with return I think)
+
+  // the day number progresses
+
+  // push info to Firebase
+
+  // display values on my scoreboard, including comments
+
+  // when day =28, display button to start over 
 
     //return false; not in my case - I WANT it to return to refresh the form
   });
 
 
+//start again button
+// on submit, clear array?
+// start at day = 1 again
 
