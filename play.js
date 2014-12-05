81a1c6c5f45851687d0da0733b6f4f4da8908c37
chanson033 <chanson033@gmail.com>
2014-12-04 18:29:15 -0800

    grand total working

--- play.js
+++ play.js
@@ -1,15 +1,16 @@
 //Cate Hanson's final project
 
 var myDataRef = new Firebase("https://fiery-inferno-6944.firebaseIO.com/");
+var grandTotal = 0;
+var counter = 0;
 
 
 $(document).ready(function() {
 
 
-
   // Set up a submit handler so that data is passed when the form is submitted
-  $("form.dailyscore").submit(function() {
-
+  $("form.dailyscore").submit(function(e) {
+    e.preventDefault();
 
     // ************************************************************
     // Push each object to Firebase, Firebase will create the array
@@ -37,14 +38,17 @@ $(document).ready(function() {
       var water = parseInt($( "select.water" ) .val() ,10);
       var other = parseInt($( "select.other" ) .val() ,10); 
       var comments = $( "input.comments" ).val();
-      var comment_pts;
+      var comment_pts; // this has no parseInt
         if ($( "input.comments" ).val() === "") {
           comment_pts = 0;
         } else {
           comment_pts = 1;
         };
-      var dailyTotal = (nutrition + workout + stretch + supplements + water + other + comment_pts);
+      var dailyTotal = parseInt(nutrition + workout + stretch + supplements + water + other + comment_pts);
+      console.log('here');
+      console.log(dailyTotal);
 
+      
       myDataRef.push({
         date: date, 
         nutrition: nutrition, 
@@ -55,108 +59,26 @@ $(document).ready(function() {
         other: other, 
         comments: comments,
         comment_pts: comment_pts,
-        dailyTotal: dailyTotal,
+        dailyTotal: dailyTotal
       });
 
-     //  // date key 
-     //  scores.date = $( "#datepicker").val ();
-     //    console.log (scores);
+      $("form")[0].reset()
 
-     //  // nutrition key
-     //  var nutrition_points = parseInt($( "select.nutrition" ).val() ,10);
-     //    scores.nutrition = nutrition_points;
-     //    console.log (scores);
-     //    // display 
-     //    $( ".day1 td:nth-of-type(2)" )
-     //    .replaceWith ("<td>" + nutrition_points + "</td>");
-     //    //.addClass( "nada" ); // could use this if I want to remove the color for a td with zero points
+     
+    
 
-     //  // workout key
-     //  var workout_pts = parseInt($( "select.workout" ) .val() ,10); 
-     //      scores.workout = workout_pts;
-     //      console.log (scores);
-     //      //display
-     //      $( ".day1 td:nth-of-type(3)" )
-     //      .replaceWith ("<td>" + workout_pts + "</td>");
-
-     //  // stretch key
-     //  var stretch_pts = parseInt($( "select.stretch" ) .val() ,10); 
-     //      scores.stretch = stretch_pts;
-     //      console.log (scores);
-     //      //display
-     //      $( ".day1 td:nth-of-type(4)" )
-     //      .replaceWith ("<td>" + stretch_pts + "</td>");
-
-     //  // supplments key
-     //  var supplements_pts = parseInt($( "select.supplements" ) .val() ,10);
-     //      scores.supplements = supplements_pts;
-     //      console.log (scores);
-     //      //display
-     //       $( ".day1 td:nth-of-type(5)" )
-     //      .replaceWith ("<td>" + supplements_pts + "</td>");
-
-     //  // water key
-     //  var water_pts = parseInt($( "select.water" ) .val() ,10); 
-     //      scores.water = water_pts;
-     //      console.log (scores);
-     //      //display
-     //       $( ".day1 td:nth-of-type(6)" )
-     //      .replaceWith ("<td>" + water_pts + "</td>");
-
-     //  // other key
-     //  var other_pts = parseInt($( "select.other" ) .val() ,10);
-     //      scores.other = other_pts;
-     //      console.log (scores);
-     //      // display
-     //      $( ".day1 td:nth-of-type(7)" )
-     //      .replaceWith ("<td>" + other_pts + "</td>");
-
-     //  //comments key
-     //  var comments = $( "input.comments" ).val();
-     //    scores.comments = comments;
-     //    console.log (scores);
-     //    // display
-     //    $( ".day1 td:nth-of-type(10)" )
-     //    .append (comments);
-
-     //  // comment points key
-     //  var comment_pts;
-     //    if ($( "input.comments" ).val() === "") {
-     //      comment_pts = 0;
-     //    } else {
-     //      comment_pts = 1;
-     //    };
-
-     //    scores.comment_pts = comment_pts;
-     //    console.log (scores);
-     //    // display
-     //    $( ".day1 td:nth-of-type(8)" )
-     //    .replaceWith ("<td>" + comment_pts + "</td>");
-      
-
-     //  // daily total key
-     //  dailyTotal = (nutrition_points + workout_pts + stretch_pts + supplements_pts + water_pts + other_pts + comment_pts);
-     //    scores.dailyTotal = dailyTotal;
-     //    console.log (scores);
-     //    // display
-     //    $( ".day1 td:nth-of-type(9)" )
-     //    .replaceWith ("<td>" + dailyTotal + "</td>");
-        
-      
-
-     // // display grand total - move to call back area 
-     //  $(".myTotal").append ("<strong>" + "&nbsp" + nutrition_points + "</strong>"); 
-
-    //reset the form fields 
-    // can't do $('.commentsInput').val(''); with all - use return? 
 
 
     // after 28 days have been entered, give some text and a start over button 
+    // counter ++; // ++ iterating by one
 
 
- return dailyTotal; // I WANT it to return to refresh the form, yes?
+    return dailyTotal; // I WANT it to return to refresh the form, yes?
     });
   });
+
+
+  
 
     // **********************************************
     // Call back from firebase and update scoreboard
@@ -185,11 +107,26 @@ $(document).ready(function() {
     myDataRef.on('child_added', function(snapshot) {
         var scores = snapshot.val();
         updateScore(scores.date, scores.nutrition, scores.workout, scores.stretch, scores.supplements, scores.water, scores.other, scores.comment_pts, scores.dailyTotal, scores.comments);
+     // update
+     addGrandTotal(scores.dailyTotal);
+     addCounter();
     });
   
-  
+ 
 
 //start again button
 // on submit, clear array?
 // start at day = 1 again
 
+function addGrandTotal (dailyTotal) {
+  //console.log("hi");
+     console.log("grandTotal is" +  grandTotal + "before");
+     grandTotal += dailyTotal;
+     console.log("grandTotal is" +  grandTotal + "now");
+     $(".myTotal").html("Total Points: " + grandTotal); 
+};
+
+function addCounter () {
+  counter ++;
+  return counter;
+};
